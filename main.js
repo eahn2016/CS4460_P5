77c7c2b875a2db9caf0eb3705880e08b06a5dd39
Eugene Ahn <eahn2016@gmail.com>
2019-04-21 03:05:04 -0400

    graph shows up at beginning

--- main.js
+++ main.js
@@ -29,6 +29,13 @@ function start() {
         var newCollege1 = "Abilene Christian University";
         var newCollege2 = "Abilene Christian University";
 
+    var colors = {"White": "#94D1FF",
+                "Black": "#6266CC",
+                "Hispanic": "#1C2199",
+                "American Indian": "#FFE8D4",
+                "Pacific Islander": "#CC8462",
+                "Biracial": "#95CCB7"};
+
         data.forEach(function(d) {
             var college1 = d.Name;
             var college2 = d.Name;
@@ -68,6 +75,30 @@ function start() {
 
         var colleges1 = Object.keys(collegeMap1);
         var colleges2 = Object.keys(collegeMap2);
+
+        var college1Races = [{"Race": "White", "Percent": collegeMap1[newCollege1].white},
+                {"Race": "Black", "Percent": collegeMap1[newCollege1].black},
+                {"Race": "Hispanic", "Percent": collegeMap1[newCollege1].hispanic},
+                {"Race": "American Indian", "Percent": collegeMap1[newCollege1].indian},
+                {"Race": "Pacific Islander", "Percent": collegeMap1[newCollege1].pacific},
+                {"Race": "Biracial", "Percent": collegeMap1[newCollege1].biracial}];
+            var pie = d3.pie()
+                .value(function(d) { return d.Percent; })
+                (college1Races);
+
+            var arc = d3.arc()
+                .outerRadius(radius - 10)
+                .innerRadius(0);
+
+            var g = svg1.selectAll("arc")
+                .data(pie)
+                .enter().append("g")
+                .attr("class", "arc");
+
+            g.append("path")
+                .attr("d", arc)
+                .style("fill", function(d) { return colors[d.data["Race"]];
+            });
 
     //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ College 1 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
@@ -114,13 +145,6 @@ function start() {
                     .enter().append("g")
                     .attr("class", "arc");
 
-                var colors = {"White": "#94D1FF",
-                            "Black": "#6266CC",
-                            "Hispanic": "#1C2199",
-                            "American Indian": "#FFE8D4",
-                            "Pacific Islander": "#CC8462",
-                            "Biracial": "#95CCB7"};
-
                 g.append("path")
                     .attr("d", arc)
                     .style("fill", function(d) { return colors[d.data["Race"]];
@@ -136,6 +160,29 @@ function start() {
             });
 
     //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ College 2 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+        var college2Races = [{"Race": "White", "Percent": collegeMap2[newCollege2].white},
+            {"Race": "Black", "Percent": collegeMap2[newCollege2].black},
+            {"Race": "Hispanic", "Percent": collegeMap2[newCollege2].hispanic},
+            {"Race": "American Indian", "Percent": collegeMap2[newCollege2].indian},
+            {"Race": "Pacific Islander", "Percent": collegeMap2[newCollege2].pacific},
+            {"Race": "Biracial", "Percent": collegeMap2[newCollege2].biracial}];
+        var pie = d3.pie()
+            .value(function(d) { return d.Percent; })
+            (college2Races);
+
+        var arc = d3.arc()
+            .outerRadius(radius - 10)
+            .innerRadius(0);
+
+        var g = svg2.selectAll("arc")
+            .data(pie)
+            .enter().append("g")
+            .attr("class", "arc");
+
+        g.append("path")
+            .attr("d", arc)
+            .style("fill", function(d) {return colors[d.data["Race"]];
+        });
 
         function changeCollege2() {
             newCollege2 = d3.select('#select2').property('value');
@@ -180,13 +227,6 @@ function start() {
                     .data(pie)
                     .enter().append("g")
                     .attr("class", "arc");
-
-                var colors = {"White": "#94D1FF",
-                            "Black": "#6266CC",
-                            "Hispanic": "#1C2199",
-                            "American Indian": "#FFE8D4",
-                            "Pacific Islander": "#CC8462",
-                            "Biracial": "#95CCB7"};
 
                 g.append("path")
                     .attr("d", arc)
