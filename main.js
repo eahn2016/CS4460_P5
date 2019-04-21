319738ee39fb3c0c37539261d36d390412d7962a
Eugene Ahn <eahn2016@gmail.com>
2019-04-20 21:41:59 -0400

    Added 2 colleges side by side

--- main.js
+++ main.js
@@ -1,39 +1,76 @@
 var width = 500;
 var height = 500;
 
-var graph = document.getElementById('graph');
+var graph1 = document.getElementById('graph1');
+var graph2 = document.getElementById('graph2');
 
-var svg = d3.select(graph)
+var svg1 = d3.select(graph1)
+    .append('svg')
+    .attr('width', width)
+    .attr('height', height);
+
+var svg2 = d3.select(graph2)
     .append('svg')
     .attr('width', width)
     .attr('height', height);
 
 d3.csv("colleges.csv", function(data) {
     var collegeMap = {};
+
     data.forEach(function(d) {
         var college = d.Name;
         collegeMap[college] = [];
         collegeMap[college].push(college);
+
+        collegeMap[college].white = +d["% White"];
+
     })
 
     var colleges = Object.keys(collegeMap).sort();
 
-    var changeCollege = function() {
-        var newCollege = d3.select('select').property('value');
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ College 1 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+    var changeCollege1 = function() {
+        var newCollege1 = d3.select('#select1').property('value');
+        var white1 = collegeMap[newCollege1].white;
     }
-    var selection = d3.select('#graph')
+    var selection1 = d3.select('#graph1')
         .append('p')
         .append('text')
         .text('Choose a College: ')
         .append('select')
-        .attr('class', 'select')
-        .on('change', changeCollege);
+        .attr('id', 'select1')
+        .on('change', changeCollege1);
 
-    var selectCollege = selection.selectAll('option')
+    var selectCollege1 = selection1.selectAll('option')
         .data(colleges)
         .enter()
         .append('option')
         .text(function(d) {
             return d;
         })
+
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ College 2 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+    var changeCollege2 = function() {
+        var newCollege2 = d3.select('#select2').property('value');
+        // var region2 = newCollege2["Region"];
+        // console.log(newCollege2.region);
+    }
+    var selection2 = d3.select('#graph2')
+        .append('p')
+        .append('text')
+        .text('Choose a College: ')
+        .append('select')
+        .attr('id', 'select2')
+        .on('change', changeCollege2);
+
+    var selectCollege2 = selection2.selectAll('option')
+        .data(colleges)
+        .enter()
+        .append('option')
+        .text(function(d) {
+            return d;
+        })
+
 	});
